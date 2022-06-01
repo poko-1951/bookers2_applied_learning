2728ec07c2574ad0e224692483f165cfffb951bc
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-06-01 17:14:01 +0900

    [Add]コメント機能を非同期通信に対応させた

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,6 +3,7 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
+    render "create_and_destroy"
     # redirect_to request.referer
   end
 
@@ -10,6 +11,7 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
+    render "create_and_destroy"
     # redirect_to request.referer
   end
   
