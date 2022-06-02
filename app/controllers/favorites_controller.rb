a7956616bf0a32c936db9533a3960044129f71a8
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-06-02 10:32:27 +0900

    [Add]1週間におけるいいね数順に表示

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,6 +3,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
+    to = Time.current.at_end_of_day
+    from = (to-6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).sort{
+      |a,b| 
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+    }
     render "create_and_destroy"
     # redirect_to request.referer
   end
@@ -11,6 +18,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
+    to = Time.current.at_end_of_day
+    from = (to-6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).sort{
+      |a,b| 
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+    }
     render "create_and_destroy"
     # redirect_to request.referer
   end
