cc94f0951d58a9da3f17dba06b23f4e7dd42d74f
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-06-04 23:58:40 +0900

    [Add]グループ作成機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,8 @@ class User < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_many :chats, dependent: :destroy
 
+  has_many :group_users, dependent: :destroy
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
@@ -21,6 +22,7 @@ Rails.application.routes.draw do
   end
 
   resources :chats, only: [:show, :create]
+  resources :groups, only: [:new, :create, :index, :show, :edit, :update]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
