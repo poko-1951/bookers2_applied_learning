f74b3654ecda40a48c4a9bdf13fdf0349d0f49d3
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-06-05 16:57:10 +0900

    [Add]5段階評価機能実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
+window.$ = window.jQuery = require('jquery');
+import "packs/book_raty"
 import "popper.js"
 import "bootstrap"
 // require("chartkick") 
